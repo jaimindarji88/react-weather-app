4cf3fa700c746a1142f43605c7648c041c8e166f
Jaimin Darji <jaimin.d.88@gmail.com>
2016-11-27 22:43:52 -0500

    added text to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 var About = (props)=>{
 	return (
 		<div>
-			<h3>About Comp</h3>
-			<p>Welcome to the about page!</p>
+			<h1 className='text-center'>About</h1>
+			<p>Weather application made using ReactJS</p>
+			<p>Here are some of the tools I used</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react" target="_blank">
+						ReactJS, Javascript Framework
+					</a>
+				</li>
+				<li>
+					<a href="http://openweathermap.org" target="_blank">
+						Open Weather Map Rest API to get the weather data by city name
+					</a>
+				</li>
+			</ul>
 		</div>
 	)
 }
